3bd54fe3d9450d9655b52f74eca0adbeac5a9d38
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-03-19 15:36:44 +0600

    english janala is completed

--- script.js
+++ script.js
@@ -18,7 +18,18 @@ const display_all_levels = (levels) => {
 
 all_levels_load();
 
+const manageSpinner = (status) => {
+  if (status == true) {
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("level_word_section").classList.add("hidden");
+  } else if (status == false) {
+    document.getElementById("level_word_section").classList.remove("hidden");
+    document.getElementById("spinner").classList.add("hidden");
+  }
+};
+
 const level_word = (level_no) => {
+  manageSpinner(true);
   fetch(`https://openapi.programming-hero.com/api/level/${level_no}`)
     .then((res) => res.json())
     .then((data) => {
@@ -63,11 +74,59 @@ const display_level_word = (words) => {
     <h1 class="font-bangla text-3xl font-semibold">${word.meaning} / ${word.pronunciation}</h1>
 
     <div class="flex justify-between items-center pt-10">
-    <span class="btn btn-soft"><i class="fa-solid fa-circle-info"></i></span>
+    <span onclick="loadWordDetails(${word.id})" class="btn btn-soft"><i class="fa-solid fa-circle-info"></i></span>
     <span class="btn btn-soft"><i class="fa-solid fa-volume-high"></i></span>
     </div>
     </div>
     `;
     level_word_section.appendChild(level_word);
   });
+  manageSpinner(false);
 };
+
+const loadWordDetails = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/word/${id}`)
+    .then((res) => res.json())
+    .then((data) => displayWordDetails(data.data));
+};
+
+const createElements = (array) => {
+  const element = array.map(
+    (arr) => `<span class="btn btn-primary btn-soft">${arr}</span>`,
+  );
+  return element.join(" ");
+};
+
+const displayWordDetails = (data) => {
+  const word_details = document.getElementById("word_details");
+  word_details.innerHTML = `
+  <h1 class="text-4xl font-semibold mb-8">${data.word} (<i class="fa-solid fa-microphone-lines"></i>:${data.pronunciation})</h1>
+        <p class="text-2xl font-semibold">Meaning</p>
+        <p class="text-2xl font-medium font-bangla mt-2.5 mb-8">${data.meaning}</p>
+        <p class="text-2xl font-semibold">Example</p>
+        <p class="text-2xl mt-2.5 mb-8">${data.sentence}</p>
+        <p class="text-2xl font-medium font-bangla">সমার্থক শব্দ গুলো</p>
+        <div class="mt-2.5 mb-8">
+            <div>${createElements(data.synonyms)}</div>
+        </div>
+        <button class="btn btn-primary">Complete Learning</button>
+  `;
+
+  document.getElementById("word_modal").showModal();
+};
+
+document.getElementById("btn_search").addEventListener("click", () => {
+  const input = document.getElementById("input_search");
+  const inputValue = input.value.trim().toLowerCase();
+  console.log(inputValue);
+
+  fetch("https://openapi.programming-hero.com/api/words/all")
+    .then((res) => res.json())
+    .then((data) => {
+      const allWord = data.data;
+      const filterWord = allWord.filter(
+        (word) => word.word.toLowerCase().includes(inputValue),
+      );
+      display_level_word(filterWord);
+    });
+});
